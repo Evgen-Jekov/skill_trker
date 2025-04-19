163a9672a0e2721ce77d629dbf85059dc07577ae
Evgen-Jekov <zekovevgen992@gmail.com>
2025-04-19 02:25:20 +0000

    connect db

--- start_app.py
+++ start_app.py
@@ -1,7 +1,9 @@
 from PyQt6.QtWidgets import QApplication
 from app.GUI.main import MainWindow
+from app.repository.db import Base, engine
 
 def start():
     app = QApplication([])
     window = MainWindow()
+    Base.metadata.create_all(bind=engine)
     app.exec()
